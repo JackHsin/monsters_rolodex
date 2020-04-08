3ee86bdf32a13a3111e530e2513383b829182d48
Jack Hsin <qk14000@gmail.com>
2020-04-08 13:08:37 +0800

    Updates

---
+++ precache-manifest.e1120a9ee748fc6bdae9b81aa4169cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2459a4f1b7f0d933c0ab55ca031e9726",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "1e40e41aeb6213825432",
+    "url": "/monsters_rolodex/static/css/main.05cad73a.chunk.css"
+  },
+  {
+    "revision": "f3bc58ad93c95ac2dfd4",
+    "url": "/monsters_rolodex/static/js/2.7158d2bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_rolodex/static/js/2.7158d2bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e40e41aeb6213825432",
+    "url": "/monsters_rolodex/static/js/main.06c1b7a4.chunk.js"
+  },
+  {
+    "revision": "7edea29aa7bf7d3d6b14",
+    "url": "/monsters_rolodex/static/js/runtime-main.2a8bf7b0.js"
+  }
+]);
